74b164ef90e215a20315ac802f2f287874b08ba7
Play_D <ranzeplay@gmail.com>
2020-02-11 13:59:40 +0800

    Add "User Details" and "Create App" entry

--- OAHub.Passport/Controllers/AccountController.cs
+++ OAHub.Passport/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using OAHub.Base.Models;
@@ -16,13 +17,19 @@ namespace OAHub.Passport.Controllers
     public class AccountController : Controller
     {
         private readonly PassportDbContext _context;
+
         private readonly UserManager<OAUser> _userManager;
+        private readonly SignInManager<OAUser> _signInManager;
+
         private readonly IJwtTokenService _jwtTokenService;
 
         public AccountController(PassportDbContext context, IJwtTokenService jwtTokenService, UserManager<OAUser> userManager, SignInManager<OAUser> signInManager)
         {
             _context = context;
+
             _userManager = userManager;
+            _signInManager = signInManager;
+
             _jwtTokenService = jwtTokenService;
         }
 
@@ -33,7 +40,7 @@ namespace OAHub.Passport.Controllers
             if (decodedData != null)
             {
                 var user = await _userManager.FindByIdAsync(decodedData.Payload.FirstOrDefault(p => p.Key == "ForUser").Value.ToString());
-                if(user == null)
+                if (user == null)
                 {
                     return "User not found";
                 }
@@ -97,6 +104,41 @@ namespace OAHub.Passport.Controllers
             }
 
             return View(model);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> My()
+        {
+            if (_signInManager.IsSignedIn(User))
+            {
+                var user = await _userManager.GetUserAsync(User);
+                if (user != null)
+                {
+                    var authorizedApps = new List<App>();
+                    var appsCreatedByUser = new List<App>();
+                    foreach(var app in _context.Apps)
+                    {
+                        if (app.GetUsersAuthorized().Contains(user.Id))
+                        {
+                            authorizedApps.Add(app);
+                        }
+                        if(app.ManagerId == user.Id)
+                        {
+                            appsCreatedByUser.Add(app);
+                        }
+                    }
+
+                    return View(new MyAccountModel
+                    {
+                        User = user,
+                        AuthorizedApps = authorizedApps,
+                        AppsCreatedByUser = appsCreatedByUser
+                    });
+                }
+            }
+
+            return Unauthorized();
         }
     }
 }
