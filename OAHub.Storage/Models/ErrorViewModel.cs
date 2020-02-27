affa3efede48cf6f48a286045a5e6f0a91e43e8e
Play_D <ranzeplay@gmail.com>
2020-02-27 14:41:08 +0800

    Create project Storage

---
+++ OAHub.Storage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OAHub.Storage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
