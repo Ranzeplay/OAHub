3fe150699e67d792c3b67f34e92e987debb7702a
Jeb Feng <ranzeplay@outlook.com>
2020-03-02 10:48:45 +0800

    Can "Create" a case

--- OAHub.Storage/Controllers/CasesController.cs
+++ OAHub.Storage/Controllers/CasesController.cs
@@ -26,7 +26,7 @@ namespace OAHub.Storage.Controllers
         {
             var user = GetUserProfile();
             var shelf = _context.Shelves.FirstOrDefault(s => s.Id == shelfId);
-            if(shelf != null && user.OwnedShelf == shelfId)
+            if (shelf != null && user.OwnedShelf == shelfId)
             {
                 var cases = new List<Case>();
                 shelf.GetOwnedCases().ForEach(element =>
@@ -43,6 +43,56 @@ namespace OAHub.Storage.Controllers
                     Shelf = shelf,
                     Cases = cases
                 });
+            }
+
+            return Unauthorized();
+        }
+
+        [HttpGet]
+        public IActionResult Create(string shelfId)
+        {
+            var user = GetUserProfile();
+            var shelf = _context.Shelves.FirstOrDefault(s => s.Id == shelfId);
+            if (shelf != null && user.OwnedShelf == shelfId)
+            {
+                return View(new CreateModel { RootShelf = new KeyValuePair<string, string>(shelf.Id, shelf.Name) });
+            }
+
+            return Unauthorized();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(string shelfId, CreateModel model)
+        {
+            var user = GetUserProfile();
+            var shelf = _context.Shelves.FirstOrDefault(s => s.Id == shelfId);
+            if (shelf != null && user.OwnedShelf == shelfId)
+            {
+                if (ModelState.IsValid)
+                {
+                    var @case = new Case
+                    {
+                        Id = Guid.NewGuid().ToString("N"),
+                        Name = model.Name,
+                        Description = model.Description,
+                        CreateTime = DateTime.UtcNow,
+                    };
+
+                    // Add case to database
+                    _context.Cases.Add(@case);
+
+                    // Make target shelf own the case
+                    var cases = shelf.GetOwnedCases();
+                    cases.Add(@case.Id);
+                    shelf.SetOwnedCases(cases);
+                    _context.Shelves.Update(shelf);
+
+                    await _context.SaveChangesAsync();
+
+                    return RedirectToAction(nameof(List), new { shelfId });
+                }
+
+                return View(model);
             }
 
             return Unauthorized();
