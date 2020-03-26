d579bd7f5027b3e1d84566e1b6f83909526f5e10
Jeb Feng <ranzeplay@outlook.com>
2020-03-26 17:47:06 +0800

    Add operation-layout for project Survey

---
+++ OAHub.Survey/Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OAHub.Survey.Controllers
+{
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+    public class DashboardController : Controller
+    {
+        public IActionResult Summary()
+        {
+            return View();
+        }
+    }
+}
