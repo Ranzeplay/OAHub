70b1c62b608433ece16cec71d2df1ce9e03faa1f
Jeb Feng <ranzeplay@outlook.com>
2020-04-09 19:47:05 +0800

    Can do a survey form, upload is not available

--- OAHub.Survey/Controllers/SolveController.cs
+++ OAHub.Survey/Controllers/SolveController.cs
@@ -3,14 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using OAHub.Survey.Data;
+using OAHub.Survey.Models.ViewModels.Solve;
 
 namespace OAHub.Survey.Controllers
 {
     public class SolveController : Controller
     {
-        public IActionResult Index()
+        private readonly SurveyDbContext _context;
+
+        public SolveController(SurveyDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public IActionResult Do(string formId)
+        {
+            var form = _context.StandardForms.FirstOrDefault(f => f.Id == formId);
+            if (form != null)
+            {
+                if (form.IsPublished)
+                {
+                    var model = new DoModel
+                    {
+                        FormId = form.Id,
+                        Title = form.Title,
+                        Questions = form.GetContent()
+                    };
+
+                    return View(model);
+                }
+            }
+
+            return NotFound();
+        }
+
+        public IActionResult Submit(string formId)
+        {
+            return BadRequest();
         }
     }
 }
---
+++ OAHub.Survey/Models/ViewModels/Solve/DoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OAHub.Survey.Models.ViewModels.Solve
+{
+    public class DoModel
+    {
+        public string FormId { get; set; }
+
+        public string Title { get; set; }
+
+        public List<KeyValuePair<Base.Models.SurveyModel.Forms.Questions.Type, object>> Questions { get; set; }
+    }
+}
