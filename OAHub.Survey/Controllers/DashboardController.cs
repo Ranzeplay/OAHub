33d4050622f5300e5f1022c4210aa40beb9b31ed
Jeb Feng <ranzeplay@outlook.com>
2020-04-15 10:01:42 +0800

    Add a simple analytics page

--- OAHub.Survey/Controllers/DashboardController.cs
+++ OAHub.Survey/Controllers/DashboardController.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using OAHub.Base.Models.SurveyModels.Forms.Standard;
 using OAHub.Survey.Data;
 using OAHub.Survey.Models;
+using OAHub.Survey.Models.ViewModels.Dashboard;
 
 namespace OAHub.Survey.Controllers
 {
@@ -33,6 +35,28 @@ namespace OAHub.Survey.Controllers
                 var list = _context.StandardForms.Where(f => f.AuthorId == user.Id).ToList();
 
                 return View(list);
+            }
+
+            return NotFound();
+        }
+
+        public IActionResult Analytics()
+        {
+            var user = GetUserProfile();
+            if (user != null)
+            {
+                var forms = _context.StandardForms.Where(f => f.AuthorId == user.Id);
+                var answers = new List<StandardAnswer>();
+                foreach (var form in forms)
+                {
+                    answers.AddRange(_context.StandardAnswers.Where(a => a.ForFormId == form.Id));
+                }
+
+                return View(new AnalyticsModel
+                {
+                    TotalFormCount = forms.LongCount(),
+                    TotalSolveCount = answers.LongCount()
+                });
             }
 
             return NotFound();
---
+++ OAHub.Survey/Models/ViewModels/Dashboard/AnalyticsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OAHub.Survey.Models.ViewModels.Dashboard
+{
+    public class AnalyticsModel
+    {
+        public long TotalFormCount { get; set; }
+
+        public long TotalSolveCount { get; set; }
+    }
+}
