c8675bda29c25fe7b913cb1e70470880b7883472
Jeb Feng <ranzeplay@outlook.com>
2020-06-30 20:16:43 +0800

    Set some model fields to Required

--- OAHub.Status/Models/ViewModels/Manage/NewPostModel.cs
+++ OAHub.Status/Models/ViewModels/Manage/NewPostModel.cs
@@ -1,6 +1,7 @@
 ﻿using OAHub.Base.Models.StatusModels;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,16 @@ namespace OAHub.Status.Models.ViewModels.Manage
 {
     public class NewPostModel
     {
+        [Required]
         public string Title { get; set; }
 
+        [Required]
         public string Description { get; set; }
 
+        [Required]
         public bool ShowOnHeader { get; set; }
 
+        [Required]
         public PostColor PostColor { get; set; }
     }
 }
